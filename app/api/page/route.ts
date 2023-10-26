e237ebeec23cfee2c93795fbe10b8ef8539a2fd4
Adolf <adolfokrah@gmail.com>
2023-10-26 10:02:55 +0200

    fix: fixed code formating with prettier

--- app/api/page/route.ts
+++ app/api/page/route.ts
@@ -1,43 +1,38 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-import { NextRequest } from "next/server";
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+import { NextRequest } from 'next/server';
 
 type UpdatePageI = {
-    name: string
-    components: object
-}
+  name: string;
+  components: object;
+};
 export async function PUT(req: Request) {
-  const { name, components } : UpdatePageI = await req.json()
- 
+  const { name, components }: UpdatePageI = await req.json();
+
   const page = await prisma.page.update({
     where: {
-      name
+      name,
     },
     data: {
       name,
-      components: JSON.stringify(components)
-    }
-  })
+      components: JSON.stringify(components),
+    },
+  });
 
-   prisma.$disconnect();
-   return Response.json(page)
- 
+  prisma.$disconnect();
+  return Response.json(page);
 }
 
-export async function GET(req: NextRequest){
-    let page = await prisma.page.findFirst({
-      where: {
-        name: req.nextUrl.searchParams.get('page') || ""
-      }
-    })
+export async function GET(req: NextRequest) {
+  let page = await prisma.page.findFirst({
+    where: {
+      name: req.nextUrl.searchParams.get('page') || '',
+    },
+  });
 
-    
-    if(page){
-      page = {...page, 
-       components: JSON.parse(page?.components || "[]")}
-    }
-   
-  
+  if (page) {
+    page = { ...page, components: JSON.parse(page?.components || '[]') };
+  }
 
-   return Response.json(page)
+  return Response.json(page);
 }
